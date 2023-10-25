bc3057d97702d24b675ad322553af050af1c10fd
Dennis <dn1607727@gmail.com>
2023-10-25 21:24:12 +0700

    almost done

--- parser_app.py
+++ parser_app.py
@@ -12,6 +12,7 @@ video_url: str = 'https://animego.org/anime//player?_allow=true'
 change_series_url: str = 'https://animego.org/anime/series?dubbing=1&provider=19&episode=&id='
 requests_iter: int = 0
 link_iter: int = 1
+titles_iter: int = 2
 
 def download_html_page(url: str) -> str | int:
     try:
@@ -50,21 +51,16 @@ def parse_links_to_movies(html: str) -> list:
                              and link.split('/')[-1] != 'anime' and check_if_link(link)]
     return filtered_links
 
-def extract_info_from_html(html: str) -> dict:
-    parser = BeautifulSoup(html, features="html.parser")
-    title: str = parser.find('div', class_ ='anime-title').find('h1').get_text()
-    dl_element: list = parser.find('dl', class_='row')
-    dt_elements: list = dl_element.find_all('dt')
-    dd_elements: list = dl_element.find_all('dd')
+def extract_info_from_html(url: str) -> dict:
+    parser = BeautifulSoup(url, features="html.parser")
+    title_element: str = parser.find('div', class_ ='anime-title')
+    title: str
+    if title_element:
+        title = title_element.find('h1').get_text()
+    else:
+        title = "Название не найдено"
     param_dict: dict = {}
-    parameter: str = ''
-    value: str = ''
     param_dict['Название'] = title
-    for dt, dd in zip(dt_elements, dd_elements):
-        parameter = dt.text.strip()
-        value = dd.text.strip()
-        correct_value: str = re.sub(r'\s+', ' ', value)
-        param_dict[parameter] = correct_value
     return param_dict
 
 def get_url_last_numbers(html: str) -> int:
@@ -76,36 +72,39 @@ def make_video_url_requests(html: str) -> tuple:
     last_number_list: list = list(map(get_url_last_numbers, links_list))
     input_before: int = video_url.rfind('/')
     requests_list: list = [video_url[:input_before]+last_number_list[i]+video_url[input_before:] for i in range(len(last_number_list))]
-    return requests_list, links_list
+    titles_list: list = [extract_info_from_html(download_html_page(link)) for link in links_list]
+    return requests_list, links_list, titles_list
 
 def extract_video_page_info(html: str) -> list[list]:
     video_urls: tuple = make_video_url_requests(html)
     requests_list: list = video_urls[requests_iter]
     links_list: list = video_urls[link_iter]
+    titles_list: list = video_urls[titles_iter]
     page_content: list = []
     for url in requests_list:
         response: requests.Response = download_html_video_page(url)
         if response is not None and 'content' in response:
             page_content.append(response['content'])
-    return page_content, links_list
+    return page_content, links_list, titles_list
 
-def extract_video_page_info_series(html) -> list:
+def extract_video_page_info_series(html: str) -> list:
     video_page_info: list[list] = extract_video_page_info(html)
     page_content: list = video_page_info[requests_iter]
+    titles_list: list = video_page_info[titles_iter]
     from_page: str = "video-player-bar-series-item d-inline-block br-4 mb-0 video-player__active"
     to_page: str = 'class="video-player-bar-series-watch text-player-gray px-3 py-2 text-nowrap cursor-pointer  modal-btn modal.ajax"'
     page_content_series: list = delete_extra_info(delete_extra_info([page_content[i][page_content[i].find(from_page):page_content[i].rfind(to_page)].split('\n') for i in range(len(page_content))]))
     page_content_series = [filter_series_list(sublist) for sublist in page_content_series if sublist]
-    return page_content_series
+    return page_content_series, titles_list
 
-def extract_video_page_info_voices(html) -> list:
+def extract_video_page_info_voices(html: str) -> list:
     video_page_info: list[list] = extract_video_page_info(html)
     page_content: list = video_page_info[requests_iter]
     page_content_voices: list = delete_extra_info(delete_extra_info([page_content[i][page_content[i].find("data-dubbing"):page_content[i].find('Kodik')].split('\n') for i in range(len(page_content))]))
     page_content_voices = [filter_voice_list(sublist) for sublist in page_content_voices]
     return page_content_voices
 
-def extract_video_page_info_player(html) -> list:
+def extract_video_page_info_player(html: str) -> list:
     video_page_info: list[list] = extract_video_page_info(html)
     page_content: list = video_page_info[requests_iter]
     page_content_players: list = delete_extra_info(delete_extra_info([page_content[i][page_content[i].find("video-dubbing"):page_content[i].rfind('video-player-toggle-item-name text-underline-hover')].split('\n') for i in range(len(page_content))]))
@@ -158,7 +157,9 @@ def delete_extra_info(buff: list[list]) -> list:
 
 def get_content_all_series(main_page_url: str) -> list[list[list]]:
     url: str = 'https://animego.org/anime/series?&'
-    page_content_series: list[list] = extract_video_page_info_series(download_html_page(main_page_url))
+    request: list[list[list]] = extract_video_page_info_series(download_html_page(main_page_url))
+    page_content_series: list[list] = request[requests_iter]
+    titles_list: list = request[1]
     anime_list: list = []
     anime_content: list = []
     for anime in page_content_series:
@@ -171,7 +172,7 @@ def get_content_all_series(main_page_url: str) -> list[list[list]]:
             if response is not None and 'content' in response:
                 anime_content_series.append(response['content'])
         anime_content.append(anime_content_series)
-    return anime_content
+    return anime_content, titles_list
 
 def get_voices_all_series(anime_content_list: list[list]) -> list[list]:
     my_anime_content_list: list[list] = anime_content_list
@@ -187,21 +188,61 @@ def get_player_all_series(anime_content_list: list[list]) -> list[list]:
 
 def get_cached_result(main_page_url: str) -> list[list[list]]:
     cache_file = "cached_result.pkl"
-
     if os.path.exists(cache_file):
-        # If the cache file exists, load the cached result
         with open(cache_file, "rb") as f:
             result = pickle.load(f)
     else:
-        # If the cache file doesn't exist, call the function and save the result
-        result = get_content_all_series(main_page_url)
+        result: list[list] = get_content_all_series(main_page_url)
         with open(cache_file, "wb") as f:
             pickle.dump(result, f)
-
     return result
 
+def voices() -> dict:
+    for i, anime in enumerate(anime_voices):
+    d_sub_voices_for_anime: dict = {}
+    d_sub_voices_for_series: dict = {}
+    for voice_series in anime:
+        dub_keys: list = [elem for elem in voice_series if 'data-dubbing' in elem]
+        voices_values: list = [voice for voice in voice_series if 'data-dubbing' not in voice]
+        d_sub_voices_for_series.update(dict(zip(dub_keys, voices_values)))
+    d_voices[i] = d_sub_voices_for_series
+
+def players() -> list:
+    for anime in anime_player:
+        player_sub_list: list = []
+        for player_series in anime:
+            dub_keys_2: list = [elem for elem in player_series if 'data-dubbing' in elem]
+            player_value: list = [voice for voice in player_series if 'data-dubbing' not in voice]
+            player_sub_list.append([dub_keys_2, player_value])
+        player_list.append(player_sub_list)
+
+def dict_players() -> dict:
+    for i, anime in enumerate(player_list):
+        d_series_player: list = []
+        for series in anime:
+            d_sub_player: dict = defaultdict(list)
+            for key, value in zip(series[0], series[1]):
+                d_sub_player[key].append(value)
+            d_sub_player = dict(d_sub_player)
+            d_series_player.append(d_sub_player)
+        d_player[i] = d_series_player
+
+def final_dict() -> dict:
+    for d_num, d_final in d_player.items():
+        subvoices = d_voices.get(d_num, {})
+        series_result: dict = {}
+        for i, d_sub_final in enumerate(d_final):
+            subresult: dict = {}
+            for key in subvoices.keys():
+                if key in d_sub_final.keys():
+                    subresult[subvoices[key]] = d_sub_final[key]
+            series_result[i] = subresult
+        result[list(titles_dict[d_num].values())[0]] = series_result
+
 if __name__ == '__main__':
-    anime_content: list[list[list]] = get_cached_result('https://animego.org/anime?sort=r.rating&direction=desc')
+    info: list = get_cached_result('https://animego.org/anime?sort=r.rating&direction=desc')
+    titles_dict: dict = info[1]
+    anime_content: list[list[list]] = info[requests_iter]
     anime_voices: list = [get_voices_all_series(anime) for anime in anime_content if anime]
     anime_player: list = [get_player_all_series(anime) for anime in anime_content if anime]
     voices_list: list = []
@@ -238,8 +279,6 @@ if __name__ == '__main__':
             d_series_player.append(d_sub_player)
         d_player[i] = d_series_player
 
-'data-player="//aniboom.one/embed/WlmXYrydK0P?episode=1&amp;translation=2"'
-'data-player="//aniboom.one/embed/WlmXYrydK0P?episode=2&amp;translation=2"'
     for d_num, d_final in d_player.items():
         subvoices = d_voices.get(d_num, {})
         series_result: dict = {}
@@ -249,18 +288,5 @@ if __name__ == '__main__':
                 if key in d_sub_final.keys():
                     subresult[subvoices[key]] = d_sub_final[key]
             series_result[i] = subresult
-        result[d_num] = series_result
-    print(result)
-
-    # for anime_player, anime_voice in zip(d_player.values(), d_voices.values()):
-    #     series_list: list = []
-    #     for d in anime_player:
-    #         sub_dict: dict = {}
-    #         for d_voice in anime_voices:
-    #             for key in d_voice.keys():
-    #                 if key in d.keys():
-    #                     sub_result: dict = {d_voice[key]: d[key]}
-    #                     sub_dict.update(sub_result)
-    #         series_list.append(sub_dict)
-    #     result[i] = series_list
+        result[list(titles_dict[d_num].values())[0]] = series_result
     print(result)
